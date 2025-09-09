710250fba47e06c83bff52bed64c19ed317fa2e8
Om Prakash <omofficials70@gmail.com>
2025-09-09 14:25:20 +0530

    Hello World code

---
+++ C_Book/01_hello.c
@@ -0,0 +1,13 @@
+// This is a simple C program that prints "hello world !" to the console.
+#include <stdio.h> 
+// Include the standard input-output library
+    int main(){
+        // Main function - execution starts here
+        printf("hello world !");
+        // Print the string to the console
+        return 0;   
+        // Return 0 to indicate successful completion
+        
+    }
+ 
+
