973f084510bdafdd8d707597046998038d7941df
Om Prakash <omofficials70@gmail.com>
2025-09-10 21:38:20 +0530

    Basic Structure of C Program

---
+++ C_Logbook/01_Boilerplate_code.c
@@ -0,0 +1,13 @@
+// This is a simple C program that explains the boilerplate code structure.
+// It includes the necessary header file, defines the main function, and prints "hello world !"
+#include <stdio.h> 
+// Include the standard input-output library
+    int main(){
+        // Main function - execution starts here
+        // Within this main function we will write our code that we want to execute
+        return 0;   
+        // Return 0 to indicate successful completion
+        
+    }
+ 
+
